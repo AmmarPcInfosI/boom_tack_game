dcc673759fd625ffd043acf43c77672e20de9c77
Ammar Al Hasani <ammarsalem2011@gmail.com>
2023-08-27 11:20:57 +0400

    Update boom_tack_game.dart

--- bin/boom_tack_game.dart
+++ bin/boom_tack_game.dart
@@ -2,14 +2,14 @@ import 'package:boom_tack_game/boom_tack_game.dart' as boom_tack_game;
 
 void main() {
   for(int i=1;i<=100;i++){
-    if((i%3==0) && (i%5==0)){
+    if(i%3==0 && i%5==0){
        print("BoomTack");
     
     }
-    else if(i%5==0){
+    else if(i%5==0 && i%3!=0){
        print("Tack"); 
     }
-    else if(i%3==0){
+    else if(i%3==0 && i%5!=0 ){
       print("Boom");  
     }
     else{
